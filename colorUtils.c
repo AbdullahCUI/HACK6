2986e0c46c6fdfd550883ca0e29eac607aaca417
Abdullah-0110 <abdullahdev88@gmail.com>
2023-12-20 22:22:30 +0500

    condition for convert color

--- colorUtils.c
+++ colorUtils.c
@@ -10,3 +10,29 @@ double max(double red , double green, double blue){
 double scale(int r){
     return (double)r/255.0;
 } 
+
+// Function: which convert the rgb Filter to CMYK filter
+int rgbToCMYK(int r, int g, int b, double *c, double *m, double *y, double *k){
+    // ERROR HANDLING: to check the input is valid or not
+    if(r>255 || r<0 ||g>255 || g<0 ||b>255 || b<0){
+        return 1;
+    }
+    // convert to 0,1 scale
+    double red = scale(r);
+    double green = scale(g);
+    double blue = scale(b);
+   
+   
+    // FORMULA: TO convert the filter from rgb to CMYK
+    *k = 1 - max(red,green,blue);
+    *c = (1 - r - *k)/(1 - *k);
+    *m = (1 - g - *k)/(1 - *k);
+    *y = (1 - b - *k)/(1 - *k);
+     //CONDITION: indicate black color
+    if(*k == 1.0 && *c == 0.0 && *m == 0.0 && *y == 0.0){
+        return 0;
+    }
+    //valid check
+    return 0;
+    
+}
